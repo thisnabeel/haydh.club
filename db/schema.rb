9356d119b7362042d40e14dd68e41116d74822b7
“thisnabeel” <rockystorm@gmail.com>
2023-02-06 00:51:30 -0800

    New Database

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20230205113542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string   "name"
