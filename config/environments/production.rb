6c9203e3f95f9c67b2ec4e72679cbe8490ad66dc
“thisnabeel” <rockystorm@gmail.com>
2023-02-06 07:19:00 -0800

    New Docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,14 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #   logger           = ActiveSupport::Logger.new(STDOUT)
+  #   logger.formatter = config.log_formatter
+  #   config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # end
+
+  config.logger = RemoteSyslogLogger.new('logsN.papertrailapp.com', "7RVGilUlEOQrrLCm3xAT")
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
