50457db9ef35990a21e1955505040542e08e3bc0
“thisnabeel” <rockystorm@gmail.com>
2023-02-05 18:02:03 -0800

    No Assets

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Haydh
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.enabled = false
+
   end
 end
